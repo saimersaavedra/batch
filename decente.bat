45feec3ff8e9788c1b81eb67c22c8b735c8e1cb5
saimersaavedra <saimeradriansr@ufps.edu.co>
2024-05-05 20:26:11 -0500

    1

--- decente.bat
+++ decente.bat
@@ -2,4 +2,10 @@
 title ***SISTEMICOS***
 chcp 65001 >nul
 
+:: comprimir y descomprimir archivos 
+
+:: directorio biblioteca --> 0. descargas 1. documento 2. imagenes 3. videos 4. musica  
+:: if en otra carpeta ?  "/" [carpeta] 2 "/" [nombre archivo con la extension]
+:: descomprimir o comprimir 
+
 pause > nul 
